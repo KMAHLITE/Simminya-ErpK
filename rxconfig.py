e352c50f815bf336056d6a16b3053c846ae0fad4
Aboubacar Fabou KMAH <kmahlite@gmail.com>
2026-07-26 19:24:11 +0000

    modification de db_url

--- rxconfig.py
+++ rxconfig.py
@@ -1,9 +1,15 @@
-import reflex as rx
 import os
+import reflex as rx
+
+# Récupération dynamique du port Render ou 10000 par défaut
+port = int(os.environ.get("PORT", 10000))
 
 config = rx.Config(
     app_name="simminya_erpk",
     db_url="sqlite:///reflex.db",
+    backend_port=port,
+    backend_host="0.0.0.0",
+    api_url="https://votre-app-render.onrender.com", # Remplacez par votre vraie URL Render
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
