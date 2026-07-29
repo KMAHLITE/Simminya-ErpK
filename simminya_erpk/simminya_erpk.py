18b65e446fbf9c559ee72710e56cf0fbcf4b8371
Aboubacar Fabou KMAH <kmahlite@gmail.com>
2026-07-29 05:20:30 +0000

    dashboard

--- simminya_erpk/simminya_erpk.py
+++ simminya_erpk/simminya_erpk.py
@@ -1,6 +1,9 @@
 import reflex as rx
 from db import init_db
-from pages import welcom, login, register, admin
+from pages.welcom import welcome
+from pages.login import login
+from pages.register import register
+from pages.admin import admin_page
 
 # Initialisation unique de la BDB au démarrage
 init_db()
@@ -8,3 +11,7 @@ init_db()
 app = rx.App()
 
 # Enregistrement des routes
+app.add_page(welcome, route="/", title="Simminya - Accueil")
+app.add_page(login, route="/login", title="Simminya - Connexion")
+app.add_page(register, route="/register", title="Simminya - Inscription")
+app.add_page(admin_page, route="/admin", title="Simminya - Dashboard")
